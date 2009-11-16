0b23dab4c01dab6019f87ebd9204d1315a4c4d76
Remy Sharp <remy@remysharp.com>
2009-11-16 00:54:41 +0000

    first commit, partially tested but early tests show the library to be working

---
+++ README.markdown
@@ -0,0 +1,54 @@
+# Foreword
+
+I've *only* just written this library and have not yet tested it in detail. Use it at your own risk - but do send me feedback if you do! :)
+
+I'll be removing this section after I've completed some tests.  Initial tests show it working (though I've not checked <code>search</code> yet).
+
+# Twitter Lib
+
+**Stand alone Twitter library for open API calls using JavaScript**
+
+twitterlib.js allows you to run a number of common API calls to Twitter, set the options in a standard format, and pass a filtering search string if required.
+
+This is a quick overview of what's in Twitterlib - further documentation to come.
+
+## API methods
+
+All API methods are called with:
+
+<pre><code>twitterlib[METHOD](subject, options, callback)</code></pre>
+
+*Note that within the callback, <code>this</code> is the twitterlib object allowing you to call utility methods within the callback.*
+
+* status - single status for a user, result is in an array
+* timeline - timeline for a user
+* list - status for members of list, *subject* is in the format: screen\_name/list\_id
+* favs - favourites for a user
+* search
+
+### Options
+
+* limit - numerical
+* page - numerical
+* filter - Twitter search filter syntax, e.g. '-RT OR -via'
+
+## Utility Methods:
+
+* next - executes the last method called incrementing the page number and firing the same callback
+* ify - object to linkify and "at"ify tweets, example usage below
+* time - to access relative help formatting the twitter time to a relative time
+* filter - stand alone filter based on Twitter's search filter syntax
+
+## Example
+
+<pre><code>var count = 0, limit = 5;
+var t = twitter.timeline('rem', { limit: 5 }, function (tweets) {
+  for (var i; i < tweets.length; i++) {
+    console.log(this.ify.clean(tweets[i].text));
+  });
+
+  count++;
+  if (count < limit) {
+    this.next();
+  }
+});</code></pre>
---
+++ twitterlib.js
@@ -0,0 +1,362 @@
+// twitterlib.js (c) 2009 Remy Sharp
+// Licensed under the terms of the MIT license.
+(function (twitter, container) {
+  var ify = function() {
+    return {
+      link: function(t) {
+        return t.replace(/[a-z]+:\/\/[a-z0-9-_]+\.[a-z0-9-_:~%&\?\/.=]+[^:\.,\)\s*$]/ig, function(m) {
+          return '<a href="' + m + '">' + ((m.length > 25) ? m.substr(0, 24) + '...' : m) + '</a>';
+        });
+      },
+      at: function(t) {
+        return t.replace(/(^|[^\w]+)\@([a-zA-Z0-9_]{1,15}(\/[a-zA-Z0-9-_]+)*)/g, function(m, m1, m2) {
+          return m1 + '@<a href="http://twitter.com/' + m2 + '">' + m2 + '</a>';
+        });
+      },
+      hash: function(t) {
+        return t.replace(/(^|[^\w'"]+)\#([a-zA-Z0-9_]+)/g, function(m, m1, m2) {
+          return m1 + '#<a href="http://search.twitter.com/search?q=%23' + m2 + '">' + m2 + '</a>';
+        });
+      },
+      clean: function(tweet) {
+        return this.hash(this.at(this.link(tweet)));
+      }
+    };
+  }();
+  
+  var time = function () {
+    return {
+      time: function (date) {
+        var hour = date.getHours(),
+            min = date.getMinutes() + "",
+            ampm = 'AM';
+    
+        if (hour == 0) {
+          hour = 12;
+        } else if (hour == 12) {
+          ampm = 'PM';
+        } else if (hour > 12) {
+          hour -= 12;
+          ampm = 'PM';
+        }
+    
+        if (min.length == 1) {
+          min = '0' + min;
+        }
+    
+        return hour + ':' + min + ' ' + ampm;
+      },
+      date: function (date) {
+        var ds = date.toDateString().split(/ /),
+            mon = monthDict[date.getMonth()],
+            day = date.getDate()+'',
+            dayi = ~~(day),
+            year = date.getFullYear(),
+            thisyear = (new Date()).getFullYear(),
+            th = 'th';
+
+        // anti-'th' - but don't do the 11th, 12th or 13th
+        if ((dayi % 10) == 1 && day.substr(0, 1) != '1') {
+          th = 'st';
+        } else if ((dayi % 10) == 2 && day.substr(0, 1) != '1') {
+          th = 'nd';
+        } else if ((dayi % 10) == 3 && day.substr(0, 1) != '1') {
+          th = 'rd';
+        }
+
+        if (day.substr(0, 1) == '0') {
+          day = day.substr(1);
+        }
+
+        return mon + ' ' + day + th + (thisyear != year ? ', ' + year : '');
+      },
+      relative: function (time_value) {
+        var values = time_value.split(" "),
+            parsed_date = Date.parse(values[1] + " " + values[2] + ", " + values[5] + " " + values[3]),
+            date = new Date(parsed_date),
+            relative_to = (arguments.length > 1) ? arguments[1] : new Date(),
+            delta = ~~((relative_to.getTime() - parsed_date) / 1000),
+            r = '';
+
+        delta = delta + (relative_to.getTimezoneOffset() * 60);
+
+        if (delta < 5) {
+          r = 'less than 5 seconds ago';
+        } else if (delta < 30) {
+          r = 'half a minute ago';
+        } else if (delta < 60) {
+          r = 'less than a minute ago';
+        } else if (delta < 120) {
+          r = '1 minute ago';
+        } else if (delta < (45*60)) {
+          r = (~~(delta / 60)).toString() + ' minutes ago';
+        } else if (delta < (2*90*60)) { // 2* because sometimes read 1 hours ago
+          r = 'about 1 hour ago';
+        } else if (delta < (24*60*60)) {
+          r = 'about ' + (~~(delta / 3600)).toString() + ' hours ago';
+        } else {
+          if (delta < (48*60*60)) {
+            r = this.time(date) + ' yesterday';
+          } else {
+            r = this.time(date) + ' ' + this.date(date);
+          }
+        }
+
+        return r;
+      }    
+    };
+  }();
+  
+  var filter = (function () {
+   return {
+     match: function (tweet, search, includeHighlighted) {
+       var i = 0, s = '', text = tweet.text.toLowerCase();
+
+       if (typeof search == "string") {
+         search = this.format(search);
+       }
+
+       // loop ignore first
+       if (search['not'].length) {
+         for (i = 0; i < search['not'].length; i++) {
+           if (text.indexOf(search['not'][i]) !== -1) {
+             return false;
+           }
+         }
+       }
+
+       if (search['and'].length) {
+         for (i = 0; i < search['and'].length; i++) {
+           s = search['and'][i];
+
+           if (s.substr(0, 3) === 'to:') {
+             if (!RegExp('^@' + s.substr(3)).test(text)) {
+               return false;
+             }
+           } else if (s.substr(0, 5) == 'from:') {
+             if (tweet.user.screen_name !== s.substr(5)) {
+               return false;
+             }
+           } else if (text.indexOf(s) === -1) {
+             return false;
+           }
+         }
+       }
+
+       if (search['or'].length) {
+         for (i = 0; i < search['or'].length; i++) {
+           s = search['or'][i];
+
+           if (s.substr(0, 3) === 'to:') {
+             if (RegExp('^@' + s.substr(3)).test(text)) {
+               return true;
+             }
+           } else if (s.substr(0, 5) == 'from:') {
+             if (tweet.user.screen_name === s.substr(5)) {
+               return true;
+             }
+           } else if (text.indexOf(search['or'][i]) !== -1) {
+             return true;
+           }
+         }
+       } else if (search['and'].length) {
+         return true;
+       }
+
+       return false;
+     },
+
+     format: function (search, caseSensitive) {
+       // search can match search.twitter.com format
+       var blocks = [], ors = [], ands = [], i = 0, negative = [], since = '', until = '';
+
+       search.replace(/(["'](.*?)["']|\S+\b)/g, function (m) {
+         m = m.replace(/^["']+|["']+$/g, '');
+         blocks.push(m);
+       });
+
+       for (i = 0; i < blocks.length; i++) {
+         if (blocks[i] == 'OR' && blocks[i+1]) {
+           ors.push(blocks[i-1].toLowerCase());
+           ors.push(blocks[i+1].toLowerCase());
+           i++;
+           ands.pop(); // remove the and test from the last loop
+         } else if (blocks[i].substr(0, 1) == '-') {
+           negative.push(blocks[i].substr(1).toLowerCase());
+         } else {
+           ands.push(blocks[i].toLowerCase());
+         }
+       }
+
+       return {
+         'or' : ors,
+         'and' : ands,
+         'not' : negative
+       };
+     },
+
+     // tweets typeof Array
+     matchTweets: function (tweets, search, includeHighlighted) {
+       var updated = [], tmp, i = 0;
+
+       if (typeof search == 'string') {
+         search = this.format(search);
+       }
+
+       for (i = 0; i < tweets.length; i++) {
+         if (this.match(tweets[i], search, includeHighlighted)) {
+           updated.push(tweets[i]);
+         }
+       }
+
+       return updated;
+     }
+   };
+  })();
+    
+  var guid = +new Date,
+      head = document.getElementsByTagName('head')[0],
+      doc = document,
+      last = {}, // memorisation object for the next method
+      URLS = {
+        search: 'http://search.twitter.com/search.json?q=%search%&page=%page|1%&rpp=%limit|100%',
+        timeline: 'http://twitter.com/statuses/user_timeline/%user%.json?count=%limit|200%&page=%page|1%',
+        listTimeline: 'http://api.twitter.com/1/%user%/lists/%list%/statuses.json?page=%page|1%&per_page=%limit|200%',
+        favs: 'http://twitter.com/favorites/%user%.json?page=%page|1%'
+      },
+      undefined;
+  
+  function load(url, options, callback) {
+    var script = doc.createElement('script');
+    if (options == undefined) options = {};
+    guid++;
+    
+    window[twitter + guid] = (function (guid, options) { // args are now private and static
+      return function (tweets) {
+        // remove original script include
+        head.removeChild(doc.getElementById(twitter + guid));
+        if (options.filter) {
+          tweets = filter.matchTweets(tweets, options.filter);
+        }
+        callback.call(container[twitter], tweets, options);
+      };
+    })(guid, options);
+    
+    script.src = url + '&callback=' + twitter + guid;
+    script.id = twitter + guid;
+    head.appendChild(script);
+  }
+  
+  function getUrl(type, options) {
+    return URLS[type].replace(/%(.*?)(\|.*?)?%/g, function (a, key, def) {
+      return options[key] === undefined ? def.substr(1) : options[key];
+    });
+  }
+  
+  function getOptions(o) {
+    var tmp;
+    if (typeof o == 'string') {
+      o = { user: o };
+    }
+    return o;
+  }
+  
+  function normaliseArgs(options, callback) {
+    if (typeof options == 'function') {
+      callback = options;
+      options = {};
+    }
+    if (options === undefined) options = {};
+    
+    return [options, callback];
+  }
+  
+  container[twitter] = {
+    // search is an exception case
+    search: function (q, options, callback) {
+      [options, callback] = normaliseArgs(options, callback);
+      last = { 
+        method: 'search',
+        arg: q,
+        options: options,
+        callback: callback
+      };
+      
+      if (options.filter) {
+        // remove filter as it conflicts with content
+        delete options.filter;
+      }
+      
+      options.search = encodeURIComponent(q);
+      
+      if (callback) load(getUrl('search', options), options, callback);
+      return this;
+    },
+    status: function (user, options, callback) { // alias function
+      [options, callback] = normaliseArgs(options, callback);
+      last = { 
+        method: 'status',
+        arg: user,
+        options: options,
+        callback: callback,
+        page: options.page || 1
+      };
+      options.limit = 1;
+      return this.timeline(user, options, callback);
+    },
+    timeline: function (user, options, callback) {
+      [options, callback] = normaliseArgs(options, callback);
+      last = { 
+        method: 'timeline',
+        arg: user,
+        options: options,
+        callback: callback,
+        page: options.page || 1
+      };
+      options.user = user;
+      if (callback) load(getUrl('timeline', options), options, callback);
+      return this;
+    },
+    list: function (list, options, callback) {
+      var parts = list.split('/');
+      [options, callback] = normaliseArgs(options, callback);
+      last = { 
+        method: 'list',
+        arg: list,
+        options: options,
+        callback: callback,
+        page: options.page || 1
+      };
+      options.user = parts[0];
+      options.list = parts[1];
+      if (callback) load(getUrl('listTimeline', options), options, callback);
+      return this;
+    },
+    favs: function (user, options, callback) {
+      [options, callback] = normaliseArgs(options, callback);
+      last = { 
+        method: 'favs',
+        arg: user,
+        options: options,
+        callback: callback,
+        page: options.page || 1
+      };
+      options.user = user;
+      if (callback) load(getUrl('favs', options), options, callback);
+      return this;
+    },
+    next: function () {
+      if (last.method) {
+        last.page++;
+        last.options.page = last.page;
+        this[last.method](last.arg, last.options, last.callback);
+      } // else we won't do anything
+      return this;
+    },
+    
+    // appending on pre-existing utilities
+    time: time,
+    ify: ify,
+    filter: filter
+  };
+})('twitterlib', this);
